6fed4f9eb5ed68be96c896bfd13d67d36fdfb911
Kevin Lozano <kevinlzomx17@gmail.com>
2025-06-25 23:22:07 -0600

    getJsonFiles

---
+++ ICM_ImportManager/Controllers/ImportController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ICM_ImportManager.Models;
+
+namespace ICM_ImportManager.Controllers
+{
+    public class ImportController
+    {
+        private readonly string _apiUrl;
+
+        public ImportController(string apiUrl)
+        {
+            _apiUrl = apiUrl;
+        }
+
+        public List<ImportModel> ReadJsonFiles(string folderPath)
+        {
+            var files = Directory.GetFiles(folderPath, "*.json");
+            var models = new List<ImportModel>();
+
+            foreach (var file in files)
+            {
+                var json = File.ReadAllText(file);
+                var model = JsonSerializer.Deserialize<ImportModel>(json);
+                if (model != null)
+                    models.Add(model);
+            }
+
+            return models;
+        }
+
+        public async Task UploadToApiAsync(ImportModel model)
+        {
+            using var client = new HttpClient();
+            var json = JsonSerializer.Serialize(model);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            await client.PostAsync(_apiUrl, content);
+        }
+    }
+}
---
+++ ICM_ImportManager/Views/MainView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using ICM_ImportManager.Controllers;
+
+namespace ICM_ImportManager.Views
+{
+    public class MainView
+    {
+        public static async Task Main(string[] args)
+        {
+            string folderPath = ConfigurationManager.AppSettings["JsonFolderPath"];
+            string apiUrl = ConfigurationManager.AppSettings["APIURL"];
+
+            var controller = new ImportController(apiUrl);
+            var models = controller.ReadJsonFiles(folderPath);
+
+            foreach (var model in models)
+            {
+                //await controller.UploadToApiAsync(model);
+                //Console.WriteLine($"Subido: {model.Name}");
+                Console.WriteLine($"Cargado: {model.Name}");
+            }
+
+            Console.WriteLine("Proceso finalizado.");
+        }
+    }
+}
